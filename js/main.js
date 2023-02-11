c6fa5ebd7bffcccb5af1df8d750eee4f462619bc
Noiziee <dima.maskva.1194@gmail.com>
2023-02-11 14:30:53 +0300

    init(theatre-1): add solution

--- js/main.js
+++ js/main.js
@@ -10,5 +10,25 @@ const largeSwiper = new Swiper('.largeSwiper', {
   watchSlidesProgress: true,
   slidesPerView: 4,
   slidesPerGroup: 1,
+  breakpoints: {
+    310: {
+      slidesPerView: 1,
+    },
+    440: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1240: {
+      slidesPerView: 4,
+    },
+    1650: {
+      slidesPerView: 4,
+    },
+  },
 });
 
+document.querySelector('#menu__toggle').addEventListener('change', function (e) {
+  document.body.style.overflow = e.target.checked === true ? 'hidden' : '';
+});
